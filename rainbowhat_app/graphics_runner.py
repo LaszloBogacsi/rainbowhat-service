c2d8f93f56db6acec9f6d6dbb3170c96a72df01d
Laszlo Bogacsi <bogacsi.laszlo@gmail.com>
2020-09-10 22:29:52 +0100

    amend file path

--- rainbowhat_app/graphics_runner.py
+++ rainbowhat_app/graphics_runner.py
@@ -7,8 +7,6 @@ from rainbowhat_app import celery
 class ScriptRunner(object):
     def run(self, command: [str]):
         print("Running task")
-        path = os.getcwd()
-        print(path)
         popen = subprocess.Popen(command, universal_newlines=True)
         stdout, stderr = popen.communicate()
         if stderr:
@@ -21,5 +19,5 @@ scriptRunner = ScriptRunner()
 @celery.task
 def run_graphics():
     print("Running task")
-    command = ["pipenv", "run", "sudo", "python", "itv_pic.py"]
+    command = ["pipenv", "run", "sudo", "python", "./rainbowhat_app/itv_pic.py"]
     scriptRunner.run(command)
