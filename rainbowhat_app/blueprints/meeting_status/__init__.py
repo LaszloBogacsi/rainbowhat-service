125cb60ce36956f24eae5417a0bdbe8155467c75
Laszlo Bogacsi <bogacsi.laszlo@gmail.com>
2020-09-11 22:26:30 +0100

    using subprocess to start and stop python script

--- rainbowhat_app/blueprints/meeting_status/__init__.py
+++ rainbowhat_app/blueprints/meeting_status/__init__.py
@@ -1,7 +1,8 @@
 from flask import Blueprint, render_template, abort
 from jinja2 import TemplateNotFound
 
-from rainbowhat_app.graphics_runner import run_graphics
+# from rainbowhat_app.graphics_runner import run_graphics
+from rainbowhat_app.graphics_runner import GraphicsExecutor
 
 meeting_status = Blueprint('meeting_status', __name__,
                            template_folder='templates')
@@ -12,25 +13,27 @@ result = None
 @meeting_status.route('/meeting', methods=['GET'])
 def show_meeting_status():
     try:
-
         return render_template('meeting/index.html', active_status="on" if result is not None else "off")
     except TemplateNotFound:
         abort(404)
 
 
+executor = GraphicsExecutor()
+
+
 @meeting_status.route('/meeting/<status>')
 def meeting_on(status: str):
-    global result
-    from flask import current_app
-    if to_state(status) and result is None:
-        result = run_graphics.delay()
-    elif not to_state(status) and result is not None:
-        current_app.logger.info(result)
-        current_app.logger.info("Terminating process")
-        result.revoke(terminate=True, signal='SIGINT')
-        result = None
+    should_start = is_on(status) and not executor.is_running()
+    should_stop = not is_on(status) and executor.is_running()
+
+    if should_start:
+        executor.start_graphics()
+    elif should_stop:
+        executor.stop_graphics()
+    else:
+        pass
     return render_template('meeting/index.html', active_status=status)
 
 
-def to_state(status) -> bool:
+def is_on(status) -> bool:
     return status.lower() == "on"
--- rainbowhat_app/graphics_runner.py
+++ rainbowhat_app/graphics_runner.py
@@ -1,23 +1,41 @@
+import shlex
 import subprocess
 import os
 
-from rainbowhat_app import celery
-
 
 class ScriptRunner(object):
+    def __init__(self) -> None:
+        super().__init__()
+        self.current_gpid = 0
+
     def run(self, command: [str]):
-        popen = subprocess.Popen(command, universal_newlines=True, preexec_fn=os.setpgrp)
-        stdout, stderr = popen.communicate()
-        print("Task running on: {}".format(popen.pid))
+        child = subprocess.Popen(command, universal_newlines=True, preexec_fn=os.setpgrp)
+        self.current_gpid = os.getpgid(child.pid)
+        print("Task running on: {}".format(child.pid))
+        stdout, stderr = child.communicate()
         if stderr:
             raise Exception("Error "+str(stderr))
 
+    def stop(self, command_fn):
+        subprocess.check_call(command_fn(self.current_gpid))
 
-scriptRunner = ScriptRunner()
 
+class GraphicsExecutor:
 
-@celery.task
-def run_graphics():
-    print("Running task")
-    command = ["pipenv", "run", "sudo", "python3", "./rainbowhat_app/itv_pic.py"]
-    scriptRunner.run(command)
+    def __init__(self) -> None:
+        super().__init__()
+        self.runner = None
+
+    def start_graphics(self):
+        self.runner = ScriptRunner()
+        command = shlex.split("pipenv run sudo python3 ./rainbowhat_app/itv_pic.py")
+        self.runner.run(command)
+        print("stariing")
+
+    def stop_graphics(self):
+        print("stopping")
+        self.runner.stop(lambda gpid: shlex.split("sudo kill -SIGINT {}".format(gpid)))
+        self.runner = None
+
+    def is_running(self):
+        return self.runner is not None
